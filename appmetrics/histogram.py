71797502a85e9e19752bb3508520ac5d94ff1a78
Antonio Valente <y3sman@gmail.com>
2014-02-08 01:03:43 +0100

    Added basic histogram with uniform reservoir metric

---
+++ appmetrics/histogram.py
@@ -0,0 +1,121 @@
+##  Module histogram.py
+##
+##  Copyright (c) 2014 Antonio Valente <y3sman@gmail.com>
+##
+##  Licensed under the Apache License, Version 2.0 (the "License");
+##  you may not use this file except in compliance with the License.
+##  You may obtain a copy of the License at
+##
+##  http://www.apache.org/licenses/LICENSE-2.0
+##
+##  Unless required by applicable law or agreed to in writing, software
+##  distributed under the License is distributed on an "AS IS" BASIS,
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##  See the License for the specific language governing permissions and
+##  limitations under the License.
+
+import random
+import threading
+
+from . import statistics, exceptions
+
+
+DEFAULT_UNIFORM_RESERVOIR_SIZE = 1028
+
+
+class UniformReservoir(object):
+    """
+    A random sampling reservoir of floating-point values. Uses Vitter's Algorithm R to produce a statistically
+    representative sample (http://www.cs.umd.edu/~samir/498/vitter.pdf)
+    """
+
+    def __init__(self, size=DEFAULT_UNIFORM_RESERVOIR_SIZE):
+        self.size = size
+        self.values = [0] * size
+        self.count = 0
+        self.lock = threading.Lock()
+
+    def add(self, value):
+        """
+        Add a value to the reservoir
+        The value will be casted to a floating-point, so a TypeError or a ValueError may be raised.
+        """
+
+        if not isinstance(value, float):
+            value = float(value)
+
+        changed = False
+
+        with self.lock:
+            if self.count < self.size:
+                self.values[self.count] = value
+                changed = True
+            else:
+                k = random.randint(0, self.count - 1)
+                if k < self.size:
+                    self.values[k] = value
+                    changed = True
+
+        self.count += 1
+
+        return changed
+
+    @property
+    def sorted_values(self):
+        """
+        Sort and return the current sample values
+        """
+        return sorted(self.values)
+
+
+class Histogram(object):
+    """A metric which calculates some statistics over the distribution of some values"""
+
+    def __init__(self, reservoir):
+        self.reservoir = reservoir
+
+    def notify(self, value):
+        """Add a new value to the metric"""
+
+        return self.reservoir.add(value)
+
+    def raw_data(self):
+        """Return the raw underlying data"""
+
+        return self.reservoir.values
+
+    def get(self):
+        """Get the computed statistics over the gathered data"""
+
+        values = self.reservoir.sorted_values
+
+        def safe(f, *args):
+            try:
+                return f(values, *args)
+            except exceptions.StatisticsError:
+                return 0.0
+
+        plevels = [50, 75, 90, 95, 99, 99.9]
+        percentiles = [safe(statistics.percentile, p) for p in plevels]
+
+        try:
+            histogram = statistics.get_histogram(values)
+        except exceptions.StatisticsError:
+            histogram = [(0, 0)]
+
+        res = dict(
+            min=values[0] if values else 0,
+            max=values[-1] if values else 0,
+            arithmetic_mean=safe(statistics.mean),
+            geometric_mean=safe(statistics.geometric_mean),
+            harmonic_mean=safe(statistics.harmonic_mean),
+            median=safe(statistics.median),
+            variance=safe(statistics.variance),
+            standard_deviation=safe(statistics.stdev),
+            skewness=safe(statistics.skewness),
+            kurtosis=safe(statistics.kurtosis),
+            percentile=zip(plevels, percentiles),
+            histogram=histogram,
+            n=len(values))
+        return res
+
---
+++ appmetrics/tests/test_histogram.py
@@ -0,0 +1,140 @@
+import random
+
+from nose.tools import assert_equal, raises, assert_almost_equal
+import mock
+
+from .. import histogram as mm
+
+
+def test_uniform_reservoir_defaults():
+    ur = mm.UniformReservoir()
+    assert_equal(ur.size, mm.DEFAULT_UNIFORM_RESERVOIR_SIZE)
+    assert_equal(ur.values, [0] * mm.DEFAULT_UNIFORM_RESERVOIR_SIZE)
+    assert_equal(ur.count, 0)
+
+
+class TestUniformReservoir(object):
+    def setUp(self):
+        self.state = random.getstate()
+        random.seed(42)
+
+        self.size = 5
+        self.ur = mm.UniformReservoir(self.size)
+
+    def tearDown(self):
+        random.setstate(self.state)
+
+    def test_add_first(self):
+        self.ur.add(1.5)
+        assert_equal(self.ur.values, [1.5, 0, 0, 0, 0])
+
+        self.ur.add(2.5)
+        assert_equal(self.ur.values, [1.5, 2.5, 0, 0, 0])
+
+        self.ur.add(3.5)
+        assert_equal(self.ur.values, [1.5, 2.5, 3.5, 0, 0])
+
+        self.ur.add(4.5)
+        assert_equal(self.ur.values, [1.5, 2.5, 3.5, 4.5, 0])
+
+        self.ur.add(5.5)
+        assert_equal(self.ur.values, [1.5, 2.5, 3.5, 4.5, 5.5])
+
+    def test_add_overflow(self):
+        for i in range(5):
+            self.ur.add(i + 1.5)
+
+        assert_equal(self.ur.values, [1.5, 2.5, 3.5, 4.5, 5.5])
+
+        self.ur.add(10)
+        assert_equal(self.ur.values, [1.5, 2.5, 3.5, 10.0, 5.5])
+
+        self.ur.add(11)
+        assert_equal(self.ur.values, [11, 2.5, 3.5, 10.0, 5.5])
+
+        self.ur.add(12)
+        assert_equal(self.ur.values, [11, 12, 3.5, 10.0, 5.5])
+
+        self.ur.add(13)
+        assert_equal(self.ur.values, [11, 13, 3.5, 10.0, 5.5])
+
+        self.ur.add(14)
+        assert_equal(self.ur.values, [11, 13, 3.5, 10.0, 5.5])
+
+        self.ur.add(15)
+        assert_equal(self.ur.values, [11, 13, 3.5, 10.0, 5.5])
+
+    def test_sorted_values(self):
+        for i in range(5):
+            self.ur.add(random.randint(1, 10))
+
+        random.seed(42)
+        assert_equal(self.ur.sorted_values, sorted(random.randint(1, 10) for i in range(5)))
+
+    @raises(TypeError)
+    def test_add_bad_type(self):
+        self.ur.add(None)
+
+
+class TestHistogram(object):
+    def setUp(self):
+        self.reservoir = mock.Mock()
+
+        self.histogram = mm.Histogram(self.reservoir)
+
+    def test_notify(self):
+        result = self.histogram.notify(1.2)
+        assert_equal(
+            self.reservoir.add.call_args_list,
+            [mock.call(1.2)])
+        assert_equal(result, self.reservoir.add.return_value)
+
+    def test_raw_data(self):
+        result = self.histogram.raw_data()
+        assert_equal(result, self.reservoir.values)
+
+    def test_get_values_zeros(self):
+        self.reservoir.sorted_values = []
+
+        expected = dict(
+            min=0,
+            max=0,
+            arithmetic_mean=0.0,
+            geometric_mean=0.0,
+            harmonic_mean=0.0,
+            median=0.0,
+            variance=0.0,
+            standard_deviation=0.0,
+            skewness=0.0,
+            kurtosis=0.0,
+            percentile=[(50, 0.0), (75, 0.0), (90, 0.0), (95, 0.0), (99, 0.0), (99.9, 0.0)],
+            histogram=[(0, 0)],
+            n=0
+        )
+
+        assert_equal(self.histogram.get(), expected)
+
+    def test_get_values(self):
+        self.reservoir.sorted_values = [1.5, 2.5, 2.5, 2.75, 3.25, 3.26, 4.75]
+
+        res = self.histogram.get()
+
+        assert_almost_equal(res['min'], 1.5)
+        assert_almost_equal(res['max'], 4.75)
+        assert_almost_equal(res['arithmetic_mean'], 2.93)
+        assert_almost_equal(res['geometric_mean'], 2.784379085700406)
+        assert_almost_equal(res['harmonic_mean'], 2.6362666258180956)
+        assert_almost_equal(res['median'], 2.75)
+        assert_almost_equal(res['variance'], 0.99513333)
+        assert_almost_equal(res['standard_deviation'], 0.9975636988851055)
+        assert_almost_equal(res['skewness'], 0.4329020512437358)
+        assert_almost_equal(res['kurtosis'], -0.8007344003569115)
+        assert_equal(res['percentile'], [(50, 2.75),
+                                         (75, 3.25),
+                                         (90, 3.26),
+                                         (95, 4.75),
+                                         (99, 4.75),
+                                         (99.9, 4.75)])
+        assert_equal(res['histogram'], [(3.5, 6), (5.5, 1), (7.5, 0)])
+        assert_equal(res['n'], len(self.reservoir.sorted_values))
+
